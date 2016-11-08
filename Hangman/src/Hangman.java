fed4e3961036f6121caf91f47ff52e26950f6fe0
cloudbase1 <cloudbase1@gmail.com>
2016-11-08 09:48:32 -0500

    Initial Hangman commit.

---
+++ Hangman/src/Hangman.java
@@ -0,0 +1,11 @@
+
+public class Hangman {
+public static HangmanGame hangmanGame = new HangmanGame();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hangmanGame.playGame();
+
+	}
+
+}
