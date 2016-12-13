b3704506d7347053afb1426aa9a59a016fecf5a5
cloudbase1 <cloudbase1@gmail.com>
2016-12-13 15:40:29 -0500

    Added Top level game file to development branch

---
+++ Hangman/src/Hangman.java
@@ -0,0 +1,12 @@
+
+public class Hangman {
+
+	public  static HangmanGame hangmanGame = new HangmanGame();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hangmanGame.playGame();
+
+	}
+
+}
